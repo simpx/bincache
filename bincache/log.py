3911240d7668ae9ff3233f82371d454b35f93756
simpx <simpxx@gmail.com>
2024-08-03 15:32:42 +0800

    update

--- bincache/log.py
+++ bincache/log.py
@@ -3,13 +3,17 @@ from config import get_config
 
 config = get_config()
 
+_logger
+
 def get_logger():
-    logger = logging.getLogger('bincache')
-    log_level = getattr(logging, config['log_level'], logging.INFO)
-    logger.setLevel(log_level)
-    if config['log_file']:
-        log_handler = logging.FileHandler(config['log_file'])
-        log_formatter = logging.Formatter('%(asctime)s - PID: %(process)d - PWD: %(pathname)s - %(message)s')
-        log_handler.setFormatter(log_formatter)
-        logger.addHandler(log_handler)
-    return logger
+    global _logger
+    if _logger is None:
+        _logger = logging.getLogger('bincache')
+        log_level = getattr(logging, config['log_level'], logging.INFO)
+        _logger.setLevel(log_level)
+        if config['log_file']:
+            log_handler = logging.FileHandler(config['log_file'])
+            log_formatter = logging.Formatter('%(asctime)s - PID: %(process)d - PWD: %(pathname)s - %(message)s')
+            log_handler.setFormatter(log_formatter)
+            _logger.addHandler(log_handler)
+    return _logger
