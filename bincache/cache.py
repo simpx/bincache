9e096ab8ac17ebfb98d5c8bea0710a1eaa4e9d73
simpx <simpxx@gmail.com>
2024-08-04 22:09:29 +0800

    trim_cache_dir_to_limit

--- bincache/cache.py
+++ bincache/cache.py
@@ -10,6 +10,30 @@ def get_cache_file_path(key):
     cache_file_folder = os.path.join(config['cache_dir'], prefix)
     return os.path.join(cache_file_folder, filename)
 
+# TODO Add file locking or other mechanism to handle multi-process access
+def trim_cache_dir_to_limit(cache_dir, limit_size_in_bytes):
+    """removing old files if the total size exceeds the limit."""
+    total_size = 0
+    file_paths = []
+    for root, dirs, files in os.walk(cache_dir):
+        # Ignore the files in the first level directory (configuration files)
+        if root == cache_dir:
+            continue
+        for name in files:
+            file_path = os.path.join(root, name)
+            file_size = os.path.getsize(file_path)
+            total_size += file_size
+            file_paths.append((file_path, os.path.getmtime(file_path), file_size))
+    if total_size <= limit_size_in_bytes:
+        return
+    # Sort files by modification time (oldest first)
+    file_paths.sort(key=lambda x: x[1])
+    for file_path, _, file_size in file_paths:
+        os.remove(file_path)
+        total_size -= file_size
+        if total_size <= limit_size_in_bytes:
+            break
+
 def put(key, value):
     config = get_config()
     cache_file_path = get_cache_file_path(key)
@@ -29,6 +53,9 @@ def put(key, value):
                 os.remove(temp_path)
     else:
         pickle.dump(value, cache_file_path)
+
+    if 'max_size' in config:
+        trim_cache_dir_to_limit(config['cache_dir'], config['max_size'])
 
 def get(key):
     if not key:
